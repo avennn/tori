8ef6de2f483a07bd880b02ef3ca2e8fb5c5d7813
liangjianwen <liangjianwen@kuaishou.com>
2021-04-16 10:08:38 +0800

    chore: code order

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as eventBus, EventBus } from './eventBus';
 export { default as getExactType } from './getExactType';
 export { default as isNil } from './isNil';
+export { default as runAtLeastDelay } from './runAtLeastDelay';
 export { default as safeJsonParse } from './safeJsonParse';
 export { default as sleep } from './sleep';
-export { default as runAtLeastDelay } from './runAtLeastDelay';
