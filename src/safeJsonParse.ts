f8279380f075ea18fb050c683e6fdcf1f46c910d
liangjianwen <liangjianwen@kuaishou.com>
2021-04-16 11:30:30 +0800

    feat: add isPlainObject method & extend safeJsonParse

--- src/safeJsonParse.ts
+++ src/safeJsonParse.ts
@@ -1,9 +1,28 @@
+import isPlainObject from './isPlainObject';
+import getExactType from './getExactType';
+
+interface IJsonParseOption {
+    force?: boolean; // force result to keep same type with defaultVal
+}
+
 /**
  * Try to parse input value correctly，return defaultVal if catch error.
  */
-export default function safeJsonParse(input: any, defaultVal?: any) {
+export default function safeJsonParse(
+    input: any,
+    defaultVal?: any,
+    options?: IJsonParseOption
+) {
     try {
-        return JSON.parse(input);
+        const result = JSON.parse(input);
+        if (
+            isPlainObject(options) &&
+            options!.force &&
+            getExactType(result) !== getExactType(defaultVal)
+        ) {
+            return defaultVal;
+        }
+        return result;
     } catch (e) {
         return defaultVal;
     }
