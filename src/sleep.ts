fefae7ac93e8fef8e46a1b09cc19f0baf580aac7
liangjianwen <liangjianwen@kuaishou.com>
2021-04-20 11:02:28 +0800

    feat: sleep sleepAtLeast callback timer id

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,11 +1,14 @@
 /**
  * wait for milliseconds and resolve timer id.
  */
-// TODO
-export default function sleep(delay: number): Promise<NodeJS.Timeout> {
+export default function sleep(
+    delay: number,
+    timerGetter?: (timer: number) => any
+): Promise<number> {
     return new Promise((resolve) => {
-        const timer = setTimeout(() => {
-            resolve(timer);
-        }, delay);
+        const timer = setTimeout(resolve, delay);
+        if (timerGetter && typeof timerGetter === 'function') {
+            timerGetter(timer);
+        }
     });
 }
--- src/sleepAtLeast.ts
+++ src/sleepAtLeast.ts
@@ -3,16 +3,26 @@ import sleep from './sleep';
 /**
  * Sleep at least delay.
  */
-export default async function sleepAtLeast(delay = 0, startTime?: number) {
+export default async function sleepAtLeast(
+    delay = 0,
+    startTime?: number,
+    timerGetter?: (timer: number) => any
+) {
     if (typeof delay !== 'number' || delay < 0) {
         throw new Error('params delay must be a positive number');
     }
+    let tg;
+    if (typeof startTime === 'function') {
+        tg = startTime;
+    } else {
+        tg = timerGetter;
+    }
     if (typeof startTime !== 'number' || startTime <= 0) {
-        return await sleep(delay);
+        return await sleep(delay, tg);
     }
     const diff = Date.now() - startTime;
     if (diff <= 0 || diff >= delay) {
-        return await sleep(0);
+        return await sleep(0, tg);
     }
-    return await sleep(delay - diff);
+    return await sleep(delay - diff, tg);
 }
